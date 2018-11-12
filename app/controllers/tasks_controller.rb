28cafb093c3105c49ae4060c17da6306f3ece92b
Alessandra Huydts <A.huydts@yahoo.de>
2018-11-12 12:16:40 +0200

    created the show

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,18 @@
 class TasksController < ApplicationController
 
+before_action :set_task, only: [:show, :edit, :update, :delete]
+
   def index
     @tasks = Task.all
   end
 
-end
+  def show
+  end
 
+private
+  def set_task
+    @task = Task.find(params[:id])
+  end
+end
 
 
