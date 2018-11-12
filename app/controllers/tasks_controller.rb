0fcbeff08ff053506dc149de5838a260b2885018
Alessandra Huydts <A.huydts@yahoo.de>
2018-11-12 12:39:49 +0200

    finished the create new task part

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,7 +9,21 @@ before_action :set_task, only: [:show, :edit, :update, :delete]
   def show
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
 private
+  def task_params
+    params.require(:task).permit(:title, :detail)
+  end
+
   def set_task
     @task = Task.find(params[:id])
   end
